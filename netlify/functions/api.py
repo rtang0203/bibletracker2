b8fbd763bf4ad405c3979c2fc46adff99e6e49d3
randy <rtang0203@gmail.com>
2025-06-21 14:05:22 -0500

    fuck netlify

--- netlify/functions/api.py
+++
@@ -1,9 +0,0 @@
-# netlify/functions/api.py
-import os
-from serverless_wsgi import handle_request
-from app import create_app 
-
-app = create_app()
-
-def handler(event, context):
-    return handle_request(app, event, context) 
